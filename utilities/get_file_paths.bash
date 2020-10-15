a030ffe417345bee0a3e34acace4893599ec1877
Timothy-Barry <timothybarry@cmu.edu>
2020-10-15 16:05:19 -0400

    update utilities

--- utilities/get_file_paths.bash
+++ utilities/get_file_paths.bash
@@ -3,9 +3,9 @@
 # This script takes as an argument (i) the name of a machine ("local", "hydra", "ubergenno", "uberduo", "bridges") and (ii) the desired directory ("code" or "data_results"). It outputs the full file path of that directory on that machine.
 
 # Define machine_name, code_dir, and data_results_dir arrays.
-machine_name=(local hydra ubergenno uberduo bridges)
-code_dir=(/Users/timbarry/Box/SCEPTRE/sceptre_paper /home/tbarry2/SCEPTRE/sceptre_paper ubergenno_code_dir uberduo_code_dir bridges_code_dir)
-data_results_dir=(/Volumes/tims_new_drive/research/sceptre_files /raid6/Tim/sceptre_offsite_dir ubergenno_data_dir uberduo_data_dir bridges_data_dir)
+machine_name=(local ubergenno hydra uberduo bridges)
+code_dir=(/Users/timbarry/Box/SCEPTRE/sceptre_paper /home/tbarry2/SCEPTRE/sceptre_paper ubergenno_code_dir hydra_code_dir bridges_code_dir)
+data_results_dir=(/Volumes/tims_new_drive/research/sceptre_files /raid6/Tim/sceptre_offsite_dir hydra_data_dir uberduo_data_dir bridges_data_dir)
 
 # find the index of the selected machine
 for ((index=0; index<${#machine_name[@]}; index++))
