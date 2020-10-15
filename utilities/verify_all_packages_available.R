45ba4393c7b299e03a12abc13c3af0250a4ccc6a
Timothy-Barry <timothybarry@cmu.edu>
2020-10-15 18:28:03 -0400

    fix verify packages script

--- utilities/verify_all_packages_available.R
+++ utilities/verify_all_packages_available.R
@@ -4,10 +4,10 @@
 verify_all_packages_available <- function(df) {
   my_packs <- rownames(installed.packages())
   for (i in 1:nrow(df)) {
-    curr_package <- df[i, "package"]
+    curr_package <- as.character(df[[i, "package"]])
     cat(paste("Checking", curr_package, "\n"))
     if(!(curr_package %in% my_packs)) {
-      curr_loc <- df[i, "loc"]
+      curr_loc <- as.character(df[[i, "loc"]])
       if (curr_loc == "CRAN")  {
         install.packages(curr_package)
       } else if (curr_loc == "Bioc") {
@@ -15,8 +15,8 @@ verify_all_packages_available <- function(df) {
         library(BiocManager)
         BiocManager::install(curr_package)
       } else if (curr_loc == "github") {
-        git_dir <- df[i, "github_repo"]
-        git_subdir <- df[i, "github_repo_subdir"]
+        git_dir <- as.character(df[[i, "github_repo"]])
+        git_subdir <- as.character(df[[i, "github_repo_subdir"]])
         if (!("devtools" %in% my_packs)) install.packages("devtools")
         library(devtools)
         if (is.na(git_subdir)) install_github(repo = git_dir) else install_github(repo = git_dir, subdir = git_subdir)
